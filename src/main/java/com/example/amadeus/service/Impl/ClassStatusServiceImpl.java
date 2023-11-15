f67c601f9895bd8b1b5162a09377b8517d6f4ec7
MollyXuemn <Molly504954366@gmail.com>
2023-11-15 10:15:42 +0100

    feat: getClassStatus

--- src/main/java/com/example/amadeus/service/Impl/ClassStatusServiceImpl.java
+++ src/main/java/com/example/amadeus/service/Impl/ClassStatusServiceImpl.java
@@ -1,9 +1,26 @@
 package com.example.amadeus.service.Impl;
 
+import com.example.amadeus.model.ClassStatus;
 import com.example.amadeus.service.ClassStatusService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClassStatusServiceImpl implements ClassStatusService {
+    @Override
+    public ClassStatus getClassStatus(int threshold, int[] times) {
+        int onTimeOrEarlyStudents = 0;
 
+        for(int arrivalTime :times){
+            if(arrivalTime <= 0){
+                onTimeOrEarlyStudents++;
+            }
+        }
+        String status = onTimeOrEarlyStudents >= threshold ? "NO" : "YES";
+        return new ClassStatus(status);
+    }
+
+    @Override
+    public double getWeekStatus(int threshold, int[][] week){
+        return 0;
+    }
 }
