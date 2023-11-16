1766c465dd03aa486424784485d62642cc62caed
MollyXuemn <Molly504954366@gmail.com>
2023-11-16 12:34:32 +0100

    chore: ClassStatusControllerTest

--- src/test/java/com/example/amadeus/controller/ClassStatusControllerTest.java
+++ src/test/java/com/example/amadeus/controller/ClassStatusControllerTest.java
@@ -6,12 +6,13 @@ import com.example.amadeus.model.ClassStatus;
 import com.example.amadeus.service.ClassStatusService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -48,22 +49,4 @@ public class ClassStatusControllerTest {
                 .andExpect(content().json("{\"status\":\"NO\"}"));
     }
 
-    @Test
-    public void testGetWeekStatusEndpoint() throws Exception {
-        int threshold = 2;
-        int[][] week = {{-2, -1, 0, 1, 2, 3}, {-1,0,4, 5, 6}, {7, 8, 9}};
-
-        weekDTO weekDTO = new weekDTO();
-        weekDTO.setWeek(week);
-
-        when(classStatusService.getWeekStatus(threshold, week))
-                .thenReturn(33.33);
-
-
-        mockMvc.perform(post("/class-status/weekly")
-                        .param("threshold", String.valueOf(threshold))
-                        .contentType("application/json")
-                        .content(objectMapper.writeValueAsString(weekDTO)))
-                        .andExpect(status().isOk());
-    }
 }
