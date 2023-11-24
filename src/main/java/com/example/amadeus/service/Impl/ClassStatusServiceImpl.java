f02cac0196dfcbe805b18f827beea1b09b073e79
MollyXuemn <Molly504954366@gmail.com>
2023-11-24 11:12:57 +0100

    docs: add function comments

--- src/main/java/com/example/amadeus/service/Impl/ClassStatusServiceImpl.java
+++ src/main/java/com/example/amadeus/service/Impl/ClassStatusServiceImpl.java
@@ -8,7 +8,11 @@ import java.text.DecimalFormat;
 
 @Service
 public class ClassStatusServiceImpl implements ClassStatusService {
-
+    /**
+     * @param threshold The number of students that must be on time or early for class to go on
+     * @param times The arrival times of each student
+     * @return A ClassStatus object with the status of the class
+     */
     @Override
     public ClassStatus getClassStatus(int threshold, int[] times) {
         int onTimeOrEarlyStudents = 0;
@@ -16,13 +20,22 @@ public class ClassStatusServiceImpl implements ClassStatusService {
         for(int arrivalTime :times){
             if(arrivalTime <= 0){
                 onTimeOrEarlyStudents++;
+                if(onTimeOrEarlyStudents>= threshold){
+                    return new ClassStatus("No");
+                }
             }
         }
+
         String status = onTimeOrEarlyStudents >= threshold ? "NO" : "YES";
 
         return new ClassStatus(status);
     }
-
+    /**
+     * @param threshold The number of students that must be on time or early for class to go on
+     * @param week Rows: The different classes
+     *            Columns: The arrival times of each student for every class in the week
+     * @return A String with the percentage of classes that were cancelled
+     */
     @Override
     public String getWeekStatus(int threshold, int[][] week){
         int cancelledClasses = 0;
